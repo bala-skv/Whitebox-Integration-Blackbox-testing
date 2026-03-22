be1dc1b8d5278c3c5a89f692451691587d53cef8
bala-skv <bala.skv@outlook.com>
2026-03-22 18:49:27 +0530

    Iteration2 : removed unused board size import missed in last commit in dice.py

--- moneypoly/moneypoly/dice.py
+++ moneypoly/moneypoly/dice.py
@@ -1,6 +1,5 @@
 """Simulates dice rolls and tracking for doubles."""
 import random
-from moneypoly.config import BOARD_SIZE
 
 class Dice:
     """Simulates a pair of six-sided dice with consecutive-doubles tracking."""
